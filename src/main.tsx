e2ac28ece379b7383d0e5eda30790ef048304360
Melih Okan <melih.okan@ogr.dpu.edu.tr>
2026-09-15 11:50:00 +0300

    Add Tailwind Vite foundation

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./style.css";
+import "./styles/tailwind.css";
 // Import Swiper styles bundle to match the old CDN swiper-bundle.min.css
 import "swiper/css/bundle";
 import { AuthProvider } from "./context/AuthContext";
